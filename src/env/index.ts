383132d0298a6dc492343627efe2f9456c84d452
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-06-15 15:22:39 -0300

    handling env with zod

--- src/env/index.ts
+++ src/env/index.ts
@@ -14,7 +14,7 @@ const _env = envSchema.safeParse(process.env)
 if (_env.success === false) {
   console.error('invalid environment variables!', _env.error.format())
 
-  throw new Error('Invalid environmnet variables.')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
